8a0501dd854f1891cdab76af173522480d47bdf7
Jan Zaydowicz <jan.zaydowicz@googlemail.com>
2020-06-02 17:16:10 +0200

    Add device selection

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-const constraints = {
-    video: true,
-    audio: true
-};
-
 window.onload = (event) => {
     const video = document.getElementById("foobar")
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
     context = new AudioContext();
-    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-        video.srcObject = stream
-        const microphone = context.createMediaStreamSource(stream);
-        const filter = context.createBiquadFilter();
-        // microphone -> filter -> destination
-        microphone.connect(filter);
-        filter.connect(context.destination);
-    });
+    navigator.mediaDevices.enumerateDevices().then((devices) => {
+        devices = devices.filter((d) => d.kind === 'audioinput');
+        console.log(devices)
+        navigator.mediaDevices.getUserMedia({
+            audio: {
+                deviceId: devices[0].deviceId
+            },
+            video: true
+        }).then((stream) => {
+            video.srcObject = stream
+            const microphone = context.createMediaStreamSource(stream);
+            const filter = context.createBiquadFilter();
+            // microphone -> filter -> destination
+            microphone.connect(filter);
+            filter.connect(context.destination);
+        });
+    });  
 };

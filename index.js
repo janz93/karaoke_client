1e5c2e6cc9ca9ee4f7c49d31a0128e42f54444a2
Jan Zaydowicz <jan.zaydowicz@googlemail.com>
2020-06-02 16:23:41 +0200

    Add microphone access

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const constraints = {
-    video: true
+    video: true,
+    audio: true
 };
 
 window.onload = (event) => {
     const video = document.getElementById("foobar")
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    context = new AudioContext();
     navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
         video.srcObject = stream
+        const microphone = context.createMediaStreamSource(stream);
+        const filter = context.createBiquadFilter();
+        // microphone -> filter -> destination
+        microphone.connect(filter);
+        filter.connect(context.destination);
     });
 };
